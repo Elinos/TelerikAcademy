1da85aa9482c7a1ba7b40ffe9d5cc5448095a0ce
Kiril Tsarnakliyski <elinos@gmail.com>
2013-06-25 01:55:44 +0300

    Task1And4

---
+++ C# 1/Exam1/Task1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+class Program
+{
+    static void Main()
+    {
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
+        int N05 = int.Parse(Console.ReadLine());
+        int N10 = int.Parse(Console.ReadLine());
+        int N20 = int.Parse(Console.ReadLine());
+        int N50 = int.Parse(Console.ReadLine());
+        int N100 = int.Parse(Console.ReadLine());
+        decimal A = decimal.Parse(Console.ReadLine());
+        decimal P = decimal.Parse(Console.ReadLine());
+
+        decimal sumOfMachineCoins = (N05 * 0.05m) + (N10 * 0.10m) + (N20 * 0.20m) + (N50 * 0.50m) + (N100 * 1m);
+        if (A - P < 0)
+        {
+            Console.WriteLine("More {0:f2}", Math.Abs(A - P));
+        }
+        else if (A - P >= 0)
+        {
+            if ((A - P) <= sumOfMachineCoins)
+            {
+                Console.WriteLine("Yes {0:f2}", sumOfMachineCoins - (A - P));
+            }
+            else
+            {
+                Console.WriteLine("No {0:f2}", Math.Abs(sumOfMachineCoins - (A - P)));
+            }
+        }
+    }
+}
---
+++ C# 1/Exam1/Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        for (int row = 0; row < n / 2; row++)
+        {
+            Console.Write(new string('.', ((n - 2) / 2) - row));
+            Console.Write('#');
+            Console.Write(new string('.', row * 2));
+            Console.Write('#');
+            Console.Write(new string('.', ((n - 2) / 2) - row));
+            Console.WriteLine();
+        }
+        for (int row = 0; row < n / 4; row++)
+        {
+            Console.Write(new string('.', row));
+            Console.Write('#');
+            Console.Write(new string('.', (n - 2) - row * 2));
+            Console.Write('#');
+            Console.Write(new string('.', row));
+            Console.WriteLine();
+        }
+        Console.WriteLine(new string('-', n));
+        for (int row = 0; row < n / 2; row++)
+        {
+            Console.Write(new string('.', row));
+            Console.Write(new string('\\', n / 2 - row));
+            Console.Write(new string('/', n / 2 - row));
+            Console.Write(new string('.', row));
+            Console.WriteLine();
+        }
+    }
+}
