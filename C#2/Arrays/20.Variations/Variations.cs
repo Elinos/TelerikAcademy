7ab5203f39d05559aab073fd327b8f28fb8d1729
Kiril Tsarnakliyski <elinos@gmail.com>
2013-07-16 20:24:46 +0300

    [20-21]

---
+++ C#2/Arrays/20.Variations/Variations.cs
@@ -0,0 +1,39 @@
+﻿//20. Write a program that reads two numbers N and 
+//K and generates all the variations of K elements 
+//from the set [1..N].
+using System;
+
+class Variations
+{
+    static void Gen01(int index, int n, int[] variations)
+    {
+        if (index == -1)
+        {
+            Print(variations);
+        }
+        else
+        {
+            for (int i = 1; i <= n; i++)
+            {
+                variations[index] = i;
+                Gen01(index - 1, n, variations);
+            }
+        }
+    }
+
+    static void Print(int[] variations)
+    {
+        Console.WriteLine("[{0}]", String.Join(", ", variations));
+    }
+
+    static void Main()
+    {
+        Console.Write("N = ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("K = ");
+        int k = int.Parse(Console.ReadLine());
+        int[] variations = new int[k];
+        Console.WriteLine("All the variations of {0} elements from the set [1..{1}]:", k, n);
+        Gen01(k - 1, n, variations);
+    }
+}
---
+++ C#2/Arrays/21.Combinations/Combinations.cs
@@ -0,0 +1,34 @@
+﻿//21. Write a program that reads two numbers N and K 
+//and generates all the combinations of K distinct 
+//elements from the set [1..N].
+using System;
+
+class Combinations
+{
+    static void Main()
+    {
+        Console.Write("N = ");
+        int n = int.Parse(Console.ReadLine());
+        Console.Write("K = ");
+        int k = int.Parse(Console.ReadLine());
+        int[] combinations = new int[k];
+        Console.WriteLine("All the combinations of {0} elements from the set [1..{1}]:", k, n);
+        GenerateCombinations(combinations, 0, 1, n);
+    }
+
+    static void GenerateCombinations(int[] arr, int index, int startNum, int n)
+    {
+        if (index >= arr.Length)
+        {
+            Console.WriteLine("[" + String.Join(", ", arr) + "]");
+        }
+        else
+        {
+            for (int i = startNum; i <= n; i++)
+            {
+                arr[index] = i;
+                GenerateCombinations(arr, index + 1, i + 1, n);
+            }
+        }
+    }
+}
