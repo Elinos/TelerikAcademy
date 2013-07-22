48b2d043c7e9c6520b8dcc4b40ef4e1f45d49a2b
Elinos <ktsarnakliyski@gmail.com>
2013-07-22 23:36:07 +0300

    4

---
+++ C#2/MultidimensionalArrays/04.ArrayBinSearch/ArrayBinSearch.cs
@@ -0,0 +1,45 @@
+﻿//Write a program, that reads from the console an array of N integers and an integer K, sorts the array
+//and using the method Array.BinSearch() finds the largest number in the array which is ≤ K.
+using System;
+
+class ArrayBinSearch
+{
+    static void Main()
+    {
+        Console.Write("Enter N: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("array[{0}] = ", i);
+            array[i] = int.Parse(Console.ReadLine());
+        }
+        Console.Write("Enter K: ");
+        int k = int.Parse(Console.ReadLine());
+        Array.Sort(array);
+        FindMaxNumber(array, k);
+    }
+  
+    private static void FindMaxNumber(int[] array, int k)
+    {
+        int maxNumberIndex = Array.BinarySearch(array, k);
+        while (maxNumberIndex < 0)
+        {
+            if (k < array[0])
+            {
+                break;
+            }
+            k--;
+            maxNumberIndex = Array.BinarySearch(array, k);
+        }
+        if (maxNumberIndex < 0)
+        {
+            Console.WriteLine("Number not found!");
+        }
+        else
+        {
+            Console.WriteLine("Number {0} found at index [{1}]",array[maxNumberIndex], maxNumberIndex);
+        }
+    }
+}
+
