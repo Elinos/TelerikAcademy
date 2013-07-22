d45c0ee9f2c1a6a980952f6010e0daee5869ddd6
Elinos <ktsarnakliyski@gmail.com>
2013-07-22 23:51:30 +0300

    5

---
+++ C#2/MultidimensionalArrays/05.SortByLength/SortByLength.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SortByLength
+{
+    static void Main()
+    {
+        string[] array = new string[]
+	{
+	    "telerik",
+	    "academy",
+	    "is",
+	    "sooo",
+	    "nice",
+	    "!!!"
+	};
+        foreach (string str in SortByLengthIe(array))
+            Console.WriteLine(str);
+    }
+
+    static IEnumerable<string> SortByLengthIe(IEnumerable<string> elements)
+    {
+        // Use LINQ to sort the array received and return a copy.
+        var sorted = from str in elements
+                     orderby str.Length ascending
+                     select str;
+        return sorted;
+    }
+}
