1d517395859ac1c8cc7cb42808decc20e937932a
Elinos <ktsarnakliyski@gmail.com>
2013-07-22 22:04:16 +0300

    update[2]

--- C#2/MultidimensionalArrays/02.MaximalSumOfSquareInMatrix/MaximalSumOfSquareInMatrixrogram.cs
+++ C#2/MultidimensionalArrays/02.MaximalSumOfSquareInMatrix/MaximalSumOfSquareInMatrixrogram.cs
@@ -7,12 +7,15 @@ class MaximalSumOfSquareInMatrixrogram
     static void Main()
     {
         //Hardcoded matrix
-        int[,] matrix = new int[,] {{1, 9, 9 ,9, 5, 6, 7, 8},
-                                    {8, 9, 9, 9, -1, 3, 5, 0},
-                                    {4, 9, 9, 9, 2, -10, 4, -9}};
+        int[,] matrix = new int[,] 
+        {
+        {1, 9, 9 ,9, 5, 6, -7, 8},
+        {8, 9, 9, 9, -1, 3, 5, 0},
+        {4, 9, 9, 9, 2, -3, 4, -9}
+        };
         FindMaxSquare(matrix);
     }
-  
+
     private static void FindMaxSquare(int[,] matrix)
     {
         int maxSum = int.MinValue;
@@ -27,7 +30,7 @@ class MaximalSumOfSquareInMatrixrogram
                     maxSum = SumOfSquare(matrix, row, col);
                     maxRow = row;
                     maxCol = col;
-                }  
+                }
             }
         }
         PrintMaxMatrix(matrix, maxRow, maxCol);
@@ -45,16 +48,16 @@ class MaximalSumOfSquareInMatrixrogram
             Console.WriteLine();
         }
     }
-  
+
     private static int SumOfSquare(int[,] matrix, int row, int col)
     {
         int sum = 0;
-        for (int i = row -1; i <= row + 1; i++)
+        for (int i = row - 1; i <= row + 1; i++)
         {
-            for (int j = col -1; j <= col +1; j++)
+            for (int j = col - 1; j <= col + 1; j++)
             {
-                sum += matrix[i, j]; 
-            }   
+                sum += matrix[i, j];
+            }
         }
         return sum;
     }
