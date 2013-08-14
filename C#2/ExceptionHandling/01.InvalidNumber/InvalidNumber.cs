33e0c8efdfff319e657bf190d6efee95491e0bc9
Elinos <ktsarnakliyski@gmail.com>
2013-08-14 22:31:22 +0300

    ExceptionHandling

---
+++ C#2/ExceptionHandling/01.InvalidNumber/InvalidNumber.cs
@@ -0,0 +1,39 @@
+﻿//Write a program that reads an integer number and calculates and prints its square root. 
+//If the number is invalid or negative, print "Invalid number". 
+//In all cases finally print "Good bye". Use try-catch-finally.
+
+using System;
+
+class InvalidNumber
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter number: ");
+            uint number = uint.Parse(Console.ReadLine());
+            Console.WriteLine("The square root of {0} is {1}", number, Math.Sqrt((double)number));
+        }
+        catch (ArgumentNullException)
+        {
+
+            Console.Error.WriteLine("Invalid Number!");
+        }
+
+        catch (FormatException)
+        {
+
+            Console.Error.WriteLine("Invalid Number!");
+        }
+        catch (OverflowException)
+        {
+
+            Console.Error.WriteLine("Invalid Number!");
+        }
+        finally
+        {
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
+
---
+++ C#2/ExceptionHandling/04.DownloadFile/DownloadFileFromTheWeb.cs
@@ -0,0 +1,43 @@
+﻿//Write a program that downloads a file from Internet (e.g. http://www.devbg.org/img/Logo-BASD.jpg) 
+//and stores it the current directory. Find in Google how to download files in C#. 
+//Be sure to catch all exceptions and to free any used resources in the finally block.
+
+using System;
+using System.Net;
+
+class DownloadFileFromTheWeb
+{
+    static void DownloadFile(string url, string nameOfFile)
+    {
+        WebClient webClient = new WebClient();
+        try
+        {
+            webClient.DownloadFile(url, nameOfFile);
+        }
+        catch (ArgumentNullException)
+        {
+            Console.Error.WriteLine("The url or name of the file is null.");
+        }
+        catch (WebException)
+        {
+            Console.Error.WriteLine("Error while downloading the file.");
+        }
+        catch (NotSupportedException)
+        {
+            Console.Error.WriteLine("This method doesn't support this functionality.");
+        }
+        finally
+        {
+            webClient.Dispose();
+        }
+
+    }
+    static void Main()
+    {
+        string url = "http://www.devbg.org/img/Logo-BASD.jpg";
+        string nameOfFile = "../../image.jpg";
+        Console.WriteLine ("Downloading file ({0})...", url);
+        DownloadFile(url, nameOfFile);
+    }
+}
+
