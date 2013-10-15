03d0c5b0bcc6369cc90b53dc1dd804f9bcf2a7a9
Elinos <ktsarnakliyski@gmail.com>
2013-10-15 20:44:45 +0300

    EMDLL

---
+++ OOP/ExMethodsDelegatesLINQ/7.TImedMethod/ExecuteMethod.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class ExecuteMethod
+{
+    public static void Method()
+    {
+        Console.WriteLine("I was called by a delegate at {0}", DateTime.Now.ToLongTimeString());
+    }
+    static void Main()
+    {
+        Timer.Execute(3, Method);
+    }
+}
+
---
+++ OOP/ExMethodsDelegatesLINQ/7.TImedMethod/Timer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public static class Timer
+{
+    public delegate void Timed();
+    
+    public static void Execute(int seconds,Timed method)
+    {
+        var lastExecution = DateTime.Now;
+        method();
+        while (true)
+        {
+            if ((DateTime.Now - lastExecution).TotalSeconds >= seconds)
+            {
+                method();
+                lastExecution = DateTime.Now;
+            }
+        }
+    }
+}
+
