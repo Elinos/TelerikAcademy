131edff4b07ed261bc4a63adf127ef122b0949b8
Elinos <ktsarnakliyski@gmail.com>
2013-10-15 20:56:11 +0300

    add 1-5

---
+++ OOP/ExMethodsDelegatesLINQ/3-5. Students/Students.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _3_5.Students
+{
+    class Students
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Students()
+            : this("Pesho", "Ivanov", 18)
+        {
+        }
+
+        public Students(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + this.FirstName + " " + this.LastName + "\t" + "Age: " + this.Age;
+        }
+
+    }
+}
---
+++ OOP/ExMethodsDelegatesLINQ/6. NumbersDevisibleBy7And3/NumbersDevisibleBy7And3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+class NumbersDevisibleBy7And3
+{
+    static void Main()
+    {
+        int[] numbers = new int[100];
+        for (int i = 0; i < 100; i++)
+            numbers[i] = i + 1;
+
+        //with lambda
+        numbers.Where(x => x % 7 == 0 && x % 3 == 0).ToList().ForEach(Console.WriteLine);
+
+        Console.WriteLine();
+
+        PrintIntsDevisibleBy7And3(numbers);
+    }
+    //with LINQ
+    public static void PrintIntsDevisibleBy7And3(int[] array)
+    {
+        (from number in array
+         where number % 3 == 0 && number % 7 == 0
+         select number).ToList().ForEach(Console.WriteLine);
+    }
+}
+
