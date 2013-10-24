78b7cc90e8fe60c65a13d54a61a33dd8ad0c573e
Elinos <ktsarnakliyski@gmail.com>
2013-10-24 08:19:49 +0300

    added

---
+++ OOP/OOP Principles I/School/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace School
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+    }
+}
