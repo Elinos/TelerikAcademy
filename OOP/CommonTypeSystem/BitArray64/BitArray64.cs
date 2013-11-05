c3b19e5d19b796b6da17cb581126a8c052abadce
Kiril Tsarnakliyski <elinos@gmail.com>
2013-11-05 20:44:36 +0200

    CTS

---
+++ OOP/CommonTypeSystem/BitArray64/BitArray64.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitArray64
+{
+    class BitArray64 : IEnumerable<int>
+    {
+        private ulong bits;
+
+        public BitArray64(ulong number)
+        {
+            this.bits = number;
+        }
+
+        
+        public int this[int index]
+        {
+            get
+            {
+                return (int)((bits >> index) & 1);
+            }
+        }
+        
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 63; i >= 0; i--)
+            {
+                yield return (int)((bits >> i) & 1);
+            }
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public override bool Equals(object obj)
+        {
+            BitArray64 comparison = obj as BitArray64;
+
+            if (comparison == null)
+            {
+                return false;
+            }
+            else 
+            {
+                return this.bits == comparison.bits;
+            }
+           
+        }
+
+        public override int GetHashCode()
+        {
+            return bits.GetHashCode();
+        }
+
+        public static bool operator ==(BitArray64 numerOne, BitArray64 numberTwo)
+        {
+            return BitArray64.Equals(numerOne, numberTwo);
+        }
+
+        public static bool operator !=(BitArray64 numerOne, BitArray64 numberTwo)
+        {
+            return !BitArray64.Equals(numerOne, numberTwo);
+        }
+    }
+}
---
+++ OOP/CommonTypeSystem/BitArray64/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitArray64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitArray64 test = new BitArray64(2000);
+            BitArray64 test2 = new BitArray64(2000);
+            Console.WriteLine(test == test2);
+
+            foreach (int item in test)
+            {
+                Console.Write(item);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(test[10]);
+        }
+    }
+}
