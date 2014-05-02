2a728604f88e3266ea8ab05f8ac06e610c988e45
Kiril Tsarnakliyski <elinos@gmail.com>
2014-05-02 14:37:28 +0300

    CS - task three and four

--- JavaScript Fundamentals/ConditionalStatements/script.js
+++ JavaScript Fundamentals/ConditionalStatements/script.js
@@ -10,7 +10,7 @@ function randomInRange(from, to) {
 
 //TaskOne
 var first = randomInRange(-10, 10),
-    second = randomInRange(-10, 10);
+  second = randomInRange(-10, 10);
 
 if (first > second) {
   var tmp = first;
@@ -23,8 +23,8 @@ insertDiv("Smaller number is " + first + " " + " the other is " + second);
 
 //TaskTwo
 var firstNumber = randomInRange(-10, 10),
-    secondNumber = randomInRange(-20, 20),
-    thirdNumber = randomInRange(-30, 30);
+  secondNumber = randomInRange(-20, 20),
+  thirdNumber = randomInRange(-30, 30);
 
 function isNegativeProduct(first, second, third) {
   var negativeCount = 0;
@@ -44,19 +44,63 @@ function isNegativeProduct(first, second, third) {
 insertDiv("Task Two:");
 if (firstNumber === 0 || secondNumber === 0 || thirdNumber === 0) {
   insertDiv("The product of " + firstNumber + ", " + secondNumber + " and " + thirdNumber +
-          " will be 0: ");
+    " will be 0: ");
 }
 else if (isNegativeProduct(firstNumber, secondNumber, thirdNumber))
   insertDiv("The product of " + firstNumber + ", " + secondNumber + " and " + thirdNumber +
-            " will be negative");
+    " will be negative");
 else {
   insertDiv("The product of " + firstNumber + ", " + secondNumber + " and " + thirdNumber +
-            " will be positive");
+    " will be positive");
 }
 
 //TaskThree
+insertDiv("Task Three:");
+
+if (firstNumber > secondNumber) {
+  if (firstNumber > thirdNumber) {
+    insertDiv("The biggest number is " + firstNumber);
+  }
+  else {
+    insertDiv("The biggest number is " + thirdNumber);
+  }
+}
+else {
+  if (secondNumber > thirdNumber) {
+    insertDiv("The biggest number is " + secondNumber);
+  }
+  else {
+    insertDiv("The biggest number is " + thirdNumber);
+  }
+}
 
 //TaskFour
+insertDiv("Task Four: ");
+
+if (firstNumber >= secondNumber && firstNumber >= thirdNumber) {
+  if (secondNumber >= thirdNumber) {
+    insertDiv("Numbers in descending order are: " + firstNumber + ", " + secondNumber + ", " + thirdNumber);
+  }
+  else {
+    insertDiv("Numbers in descending order are: " + firstNumber + ", " + thirdNumber + ", " + secondNumber);
+  }
+}
+else if (secondNumber >= firstNumber && secondNumber >= thirdNumber) {
+  if (thirdNumber >= firstNumber) {
+    insertDiv("Numbers in descending order are: " + secondNumber + ", " + thirdNumber + ", " + firstNumber);
+  }
+  else {
+    insertDiv("Numbers in descending order are: " + secondNumber + ", " + firstNumber + ", " + thirdNumber);
+  }
+}
+else {
+  if (firstNumber >= secondNumber) {
+    insertDiv("Numbers in descending order are: " + thirdNumber + ", " + firstNumber + ", " + secondNumber);
+  }
+  else {
+    insertDiv("Numbers in descending order are: " + thirdNumber + ", " + secondNumber + ", " + firstNumber);
+  }
+}
 
 //TaskFive
 
