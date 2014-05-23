8007ab73ee481718e3b59ea957e1a02575a3559a
Kiril Tsarnakliyski <elinos@gmail.com>
2014-05-23 18:58:38 +0300

    KPK - Using control structures and Using variables, date structures and constants

---
+++ KPK/UsingControlStructures/ArrayChecker/ContainsChecker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayChecker
+{
+    class ContainsChecker
+    {
+        static bool Contains(int[] array, int expectedValue)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (array[i * 10] == expectedValue)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static void Main()
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5 };
+
+            if (Contains(array, 3))
+            {
+                Console.WriteLine("Value Found");
+            }
+        }
+    }
+}
