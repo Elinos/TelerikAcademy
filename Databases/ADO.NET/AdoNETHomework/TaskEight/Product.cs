cd4e4b9633899a77ff44f70d4d3654ddca249219
Kiril Tsarnakliyski <elinos@gmail.com>
2014-08-27 16:47:05 +0300

    AdoNet - Task 8

---
+++ Databases/ADO.NET/AdoNETHomework/TaskEight/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace TaskEight
+{
+    class Product
+    {
+
+        public Product(string name)
+        {
+            this.Name = name;
+            
+        }
+
+        public string Name { get; set; }
+    }
+}
---
+++ Databases/ADO.NET/AdoNETHomework/TaskEight/TaskEigth.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace TaskEight
+{
+    class TaskEigth
+    {
+        private static SqlConnection dbCon;
+
+        private static void ConnectToDB()
+        {
+            ConnectionStringSettings dBConnectionString = ConfigurationManager.ConnectionStrings["SQLDB"];
+            dbCon = new SqlConnection(dBConnectionString.ConnectionString);
+            dbCon.Open();
+        }
+
+        private static void DisconnectFromDB()
+        {
+            if (dbCon != null)
+            {
+                dbCon.Close();
+            }
+        }
+
+        public static void Main()
+        {
+            try
+            {
+                ConnectToDB();
+                string pattern = "ch";
+                List<Product> products = GetAllProducts(pattern);
+                foreach (var product in products)
+                {
+                    Console.WriteLine(product.Name);
+                }
+
+            }
+            finally
+            {
+                DisconnectFromDB();
+            }
+        }
+
+        private static List<Product> GetAllProducts(string pattern)
+        {
+            var products = new List<Product>();
+            string commandText = "SELECT * FROM Products WHERE CHARINDEX(@match, ProductName) > 0";
+            SqlCommand retrieveCommand = new SqlCommand(commandText, dbCon);
+            retrieveCommand.Parameters.AddWithValue("@match", pattern);
+            SqlDataReader reader = retrieveCommand.ExecuteReader();
+            using (reader)
+            {
+                while (reader.Read())
+                {
+                    string name = (string)reader["ProductName"];
+                    Product product = new Product(name);
+                    products.Add(product);
+                }
+                return products;
+            }
+        }
+    }
+}
