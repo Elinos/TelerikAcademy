e24ce06e12b052f94477d1a3d1bc012d6773a1ea
Kiril Tsarnakliyski <elinos@gmail.com>
2014-08-28 19:07:19 +0300

    Trees - Task Two and Three

---
+++ Data Structures and Algorithms/Trees And Traversals/TreesAndTraversals/TaskThree/TaskThree.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace TaskThree
+{
+    class TaskThree
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tree is building, please wait for few seconds...");
+            DirectoryTree dirTree = new DirectoryTree(@"C:\Windows");
+
+            Console.WriteLine("Done.");
+
+            Console.WriteLine("\nTree size: ");
+            double sizeInMB = dirTree.CalculateSizeOfTree();
+            Console.WriteLine("{0} megabytes", sizeInMB);
+            Console.WriteLine("{0} or gigabytes", sizeInMB / 1024);
+
+            Console.WriteLine("\nCalculate size of tree folder and subfolders: ");
+            sizeInMB = dirTree.CalculateSizeOfFolder("Cursors");
+            Console.WriteLine("{0} megabytes", sizeInMB);
+            Console.WriteLine("{0} or gigabytes", sizeInMB / 1024);
+        }
+    }
+}
---
+++ Data Structures and Algorithms/Trees And Traversals/TreesAndTraversals/TaskTwo/TaskTwo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace TaskTwo
+{
+    class TaskTwo
+    {
+        static void Main(string[] args)
+        {
+            DirSearch(@"C:\Windows");
+        }
+        static void DirSearch(string dir)
+        {
+            try
+            {
+                foreach (string f in Directory.GetFiles(dir))
+                {
+                    if (f.EndsWith(".exe"))
+                    {
+                        Console.WriteLine(Path.GetFileName(f));                    
+                    }
+                }
+                foreach (string d in Directory.GetDirectories(dir))
+                {
+                    DirSearch(d);
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
